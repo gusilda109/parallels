7bb5f03b568c0d983a91743a0da47e0bf266084a
Максим Ерченко <maksimerchenko@MacBook-Air-Maksim.local>
2026-05-26 14:07:22 +0700

    fix

--- sine_sum/src/main.cpp
+++ sine_sum/src/main.cpp
@@ -12,11 +12,21 @@ const size_t N = 10000000;
 
 int main() {
     std::vector<real_t> data(N);
-    real_t sum = 0;
+
     for (size_t i = 0; i < N; ++i) {
         data[i] = std::sin(2.0 * M_PI * i / N);
-        sum += data[i];
     }
+
+
+    real_t sum = 0;
+    real_t c   = 0;
+    for (size_t i = 0; i < N; ++i) {
+        real_t y = data[i] - c;    
+        real_t t = sum + y;   
+        c = (t - sum) - y;        
+        sum = t;                    
+    }
+
     std::cout << "Sum: " << sum << std::endl;
     return 0;
 }
